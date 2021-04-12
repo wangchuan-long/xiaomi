da1c46e1bec0d37fd798417ee4b2875b7d7bfa83
honey陈 <2874654298@qq.com>
2021-04-12 20:53:53 +0800

    陈培培第一天

--- src/router/index.js
+++ src/router/index.js
@@ -3,69 +3,84 @@ import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 const router = new VueRouter({
-  mode: "hash",
-  routes: [
-    {
-      path: "/",
-      redirect: "/home",
-    },
-    {
-      path: "/register",
-      name: "Register",
-      component: () => import("../views/register"),
-      meta: {
-        title: "注册",
-        showTab: false,
-      },
-    },
-    {
-      path: "/login",
-      name: "Login",
-      component: () => import("../views/login"),
-      meta: {
-        title: "登录",
-        showTab: false,
-      },
-    },
-    {
-      path: "/home",
-      name: "Home",
-      component: () => import("../views/home"),
-      meta: {
-        title: "首页",
-        showTab: true,
-      },
-    },
-    {
-      path: "/category",
-      name: "Category",
-      component: () => import("../views/category"),
-      meta: {
-        title: "分类",
-        showTab: true,
-      },
-    },
-    {
-      path: "/cart",
-      name: "Cart",
-      component: () => import("../views/cart"),
-      meta: {
-        title: "购物车",
-        showTab: true,
-      },
-    },
-    {
-      path: "/mine",
-      name: "Mine",
-      component: () => import("../views/mine"),
-      meta: {
-        title: "我的",
-        showTab: true,
-      },
-    },
-  ],
-  linkActiveClass: "lac",
-  linkExactActiveClass: "leac",
+    mode: "hash",
+    routes: [{
+            path: "/",
+            redirect: "/home",
+        },
+        {
+            path: "/register",
+            name: "Register",
+            component: () =>
+                import ("../views/register"),
+            meta: {
+                title: "注册",
+                showTab: false,
+            },
+        },
+        {
+            path: "/login",
+            name: "Login",
+            component: () =>
+                import ("../views/login"),
+            meta: {
+                title: "登录",
+                showTab: false,
+            },
+        },
+        {
+            path: "/home",
+            name: "Home",
+            component: () =>
+                import ("../views/home"),
+            meta: {
+                title: "首页",
+                showTab: true,
+            },
+        },
+        {
+            path: "/category",
+            name: "Category",
+            component: () =>
+                import ("../views/category"),
+            meta: {
+                title: "分类",
+                showTab: true,
+            },
+        },
+        {
+            path: "/Detail",
+            name: "detail",
+            component: () =>
+                import ("../views/list/Detail.vue"),
+            meta: {
+                title: "列表",
+                showTab: false,
+            },
+        },
+        {
+            path: "/cart",
+            name: "Cart",
+            component: () =>
+                import ("../views/cart"),
+            meta: {
+                title: "购物车",
+                showTab: true,
+            },
+        },
+        {
+            path: "/mine",
+            name: "Mine",
+            component: () =>
+                import ("../views/mine"),
+            meta: {
+                title: "我的",
+                showTab: true,
+            },
+        },
+    ],
+    linkActiveClass: "lac",
+    linkExactActiveClass: "leac",
 });
 
 export default router;
--- src/views/category.vue
+++ src/views/category.vue
@@ -1,23 +1,43 @@
 <template>
-  <div class="category">category</div>
+  <div class=''>
+     <router-link :to="{name:'detail'}">ooooooo</router-link>
+  </div>
 </template>
 
 <script>
-export default {
-  components: {},
-  data() {
-    return {};
-  },
-  computed: {},
-  watch: {},
+  export default {
+    components: {
 
-  methods: {},
-  created() {},
-  mounted() {},
-  beforeCreate() {},
-  beforeMount() {},
-  beforeUpdate() {},
-  updated() {},
-};
+    },
+    data () {
+      return {
+
+      };
+    },
+    computed: {
+
+    },
+    watch: {
+
+    },
+    methods: {
+        
+    },
+    created() {
+
+},
+    mounted() {
+
+},
+beforeCreate() {}, //生命周期 - 创建之前
+beforeMount() {}, //生命周期 - 挂载之前
+beforeUpdate() {}, //生命周期 - 更新之前
+updated() {}, //生命周期 - 更新之后
+beforeDestroy() {}, //生命周期 - 销毁之前
+destroyed() {}, //生命周期 - 销毁完成
+activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+  }
 </script>
-<style scoped></style>
+
+<style  scoped>
+</style>
