1bc93a67ec0df68cb8e5955ac4c077eb36694d6f
wangchuan <1021239673@qq.com>
2021-04-12 20:42:24 +0800

    李亚培第一天

--- src/components/footer/index.vue
+++ src/components/footer/index.vue
@@ -2,9 +2,9 @@
   <div class="">
     <van-tabbar v-model="active">
       <van-tabbar-item to="/home" icon="home-o">主页</van-tabbar-item>
-      <van-tabbar-item to="/category" icon="search">分类</van-tabbar-item>
-      <van-tabbar-item to="/cart" icon="friends-o">购物车</van-tabbar-item>
-      <van-tabbar-item to="/mine" icon="setting-o">我的</van-tabbar-item>
+      <van-tabbar-item to="/category" icon="apps-o">分类</van-tabbar-item>
+      <van-tabbar-item to="/cart" icon="cart-o">购物车</van-tabbar-item>
+      <van-tabbar-item to="/mine" icon="contact">我的</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
--- src/utils/common.js
+++ src/utils/common.js
@@ -1,1 +1,1 @@
-export const serverURL = "http://localhost:3009";
+export const serverURL = "http://192.168.12.8:3009";
