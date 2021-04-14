628c809a067a983be508e283ae0716290bf55efd
guanchun <55989878@qq.com>
2021-04-14 10:50:23 +0800

    666

--- src/router/index.js
+++ src/router/index.js
@@ -78,8 +78,8 @@ const router = new VueRouter({
       },
     },
     {
-      path: "/Detail",
-      name: "detail",
+      path: "/detail",
+      name: "Detail",
       component: () => import("../views/detail"),
       meta: {
         title: "列表",
@@ -151,19 +151,19 @@ const router = new VueRouter({
 });
 
 //解决编程式路由往同一地址跳转时会报错的情况
-// const originalPush = VueRouter.prototype.push;
-// const originalReplace = VueRouter.prototype.replace;
-// //push
-// VueRouter.prototype.push = function push(location, onResolve, onReject) {
-//   if (onResolve || onReject)
-//     return originalPush.call(this, location, onResolve, onReject);
-//   return originalPush.call(this, location).catch((err) => err);
-// };
-// //replace
-// VueRouter.prototype.replace = function push(location, onResolve, onReject) {
-//   if (onResolve || onReject)
-//     return originalReplace.call(this, location, onResolve, onReject);
-//   return originalReplace.call(this, location).catch((err) => err);
-// };
+const originalPush = VueRouter.prototype.push;
+const originalReplace = VueRouter.prototype.replace;
+//push
+VueRouter.prototype.push = function push(location, onResolve, onReject) {
+  if (onResolve || onReject)
+    return originalPush.call(this, location, onResolve, onReject);
+  return originalPush.call(this, location).catch((err) => err);
+};
+//replace
+VueRouter.prototype.replace = function push(location, onResolve, onReject) {
+  if (onResolve || onReject)
+    return originalReplace.call(this, location, onResolve, onReject);
+  return originalReplace.call(this, location).catch((err) => err);
+};
 
 export default router;
